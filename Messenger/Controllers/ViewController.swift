687d8cffd287cbd2b353b51af5a464dffd0b47bc
İsmail Parlak <ismprl9467@gmail.com>
2023-12-11 15:15:30 +0300

    Initial Commit

---
+++ Messenger/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by İsmail Parlak on 11.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
